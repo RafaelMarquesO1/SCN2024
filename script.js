4a9d93f5ba4ab97ca370657804c261217532d3b8
RafaelMarquesO1 <rafaelmarquesm.0209@gmail.com>
2024-04-26 00:07:08 -0300

    Teste de cards para colocar os cultos

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ $(document).ready(function() {
     // Quando o documento estiver pronto
     $('.fade-in').addClass('loaded'); // Adiciona a classe 'loaded' para iniciar o efeito fade-in
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var myCarousel = document.querySelector('#carouselExample');
+    var carousel = new bootstrap.Carousel(myCarousel, {
+        interval: 5000,
+        wrap: true,
+        pause: 'hover'
+    });
+
+    myCarousel.querySelector('.carousel-item').classList.add('active');
+});
